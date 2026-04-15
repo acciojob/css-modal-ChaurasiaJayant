71108b33772ed6c0c72e17fc7951db44ec709c22
ChaurasiaJayant <chaurasiajayant087@gmail.com>
2026-04-15 15:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const openBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close-modal");
+
+// open modal
+openBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+// close via button
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// close when clicking outside
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
